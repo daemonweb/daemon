514e1146386a532e68963512ce7afd168c890c26
Josh Elias <code.5jzcm@simplelogin.com>
2022-11-08 11:14:27 -0500

    add card component

--- apps/astro-solid/src/components/Title.tsx
+++ apps/astro-solid/src/components/Title.tsx
@@ -3,7 +3,7 @@ import type { Component } from "solid-js";
 
 const Title: Component = () => {
     return (
-        <h1>Clover Store Test</h1>
+        <h1 class="text-4xl text-red-400 tracking-widest">Clover Inventory</h1>
     );
 }
 
