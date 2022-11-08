beec23c4b6797a4bf6e2e0756cfc8fde64a0dbb0
Josh Elias <code.5jzcm@simplelogin.com>
2022-11-08 13:49:44 -0500

    render cards

--- apps/astro-solid/src/components/Inventory.tsx
+++ apps/astro-solid/src/components/Inventory.tsx
@@ -1,5 +1,6 @@
 import { OpenAPI, InventoryService, Item } from "../../clover";
 import { Component, createSignal, onMount } from "solid-js";
+import Card from "./Card";
 
 OpenAPI.TOKEN = "7d0d2a09-d485-0c5b-7b4d-1c8eb227446e";
 const _inventory = (await InventoryService.inventoryGetItems("X6A22CBB325M1")).elements as Item[];
@@ -16,14 +17,7 @@ const Inventory: Component = () => {
     return (
         <div>
             {inventory().map((item) => (
-                <li class="bg-white p-4">
-                    <span class="inline-block mb-2 text-lg font-bold">
-                    {item.name}
-                    </span>
-                    <span class="inline-block mb-2">
-                    Price: ${item.price}
-                    </span>
-                </li>
+                <Card item={item}></Card>
             ))}
         </div>
     );
