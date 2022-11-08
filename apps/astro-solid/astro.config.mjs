f5178b37386d43cc09d11050ae505efe190c163d
Josh Elias <code.5jzcm@simplelogin.com>
2022-11-07 22:47:18 -0500

    build ssr with astro vercel adapter

--- apps/astro-solid/astro.config.mjs
+++ apps/astro-solid/astro.config.mjs
@@ -7,6 +7,13 @@ import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), tailwind()]
+  integrations: [solidJs(), tailwind()],
+  output: "server",
+  adapter: vercel()
 });
