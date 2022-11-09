49b6021d16eb9e315f8e150bfae38e4f1f990119
Josh Elias <code.5jzcm@simplelogin.com>
2022-11-08 21:08:53 -0500

    improve card styles

--- apps/astro-solid/src/components/Card.tsx
+++ apps/astro-solid/src/components/Card.tsx
@@ -5,15 +5,32 @@ export type CardProps = {
     item: Item
 }
 
+const getItemThumbnailUrl = (itemId: string | undefined): string => {
+    return (itemId) 
+        ? `https://cloverstatic.com/menu-assets/items/${itemId}_576x576.jpeg`
+        : "https://picsum.photos/120"
+}
+
+const getPrice = (price: Number): string => {
+    if(price < 100) {
+        return "$0.00";
+    }
+    const rawString = price.toString();
+    const i = rawString.length-2
+    return `$${rawString.substring(0, i)}.${rawString.substring(i, rawString.length)}`
+}
+
 const Card: Component<CardProps> = (props) => {
     return (
-        <div class="card card-side h-56 bg-base-100 shadow-xl">
-            <figure><img src="https://placeimg.com/200/280/arch" alt="Movie"/></figure>
+        <div class="card card-side h-48 bg-base-100 shadow-xl">
+            <figure class="h-full w-48">
+                <div class="bg-cover bg-center" style={`background-image: url('${getItemThumbnailUrl(props.item.id)}');`}></div>
+            </figure>
             <div class="card-body">
                 <h2 class="card-title">{props.item.name}</h2>
-                <p>${props.item.cost}</p>
+                <p>{getPrice(props.item.price)}</p>
                 <div class="card-actions justify-end">
-                <button class="btn btn-primary">Add To Cart</button>
+                <button class="btn btn-green">Add To Cart</button>
                 </div>
             </div>
         </div>
--- apps/astro-solid/src/components/Inventory.tsx
+++ apps/astro-solid/src/components/Inventory.tsx
@@ -1,10 +1,14 @@
 import { OpenAPI, InventoryService, Item } from "../../clover";
-import { Component, createSignal, onMount } from "solid-js";
+import { Component, createSignal } from "solid-js";
 import Card from "./Card";
 
-OpenAPI.TOKEN = "7d0d2a09-d485-0c5b-7b4d-1c8eb227446e";
-const _inventory = (await InventoryService.inventoryGetItems("X6A22CBB325M1")).elements as Item[];
-
+OpenAPI.BASE = import.meta.env.CLOVER_BASE_URL;
+OpenAPI.TOKEN = import.meta.env.CLOVER_API_KEY;
+const _inventory = (await InventoryService.inventoryGetItems(
+        import.meta.env.CLOVER_MERCHANT_ID
+    ))
+    .elements
+    .filter((item: Item) => item.autoManage) as Item[];
 
 const Inventory: Component = () => {
 
@@ -12,11 +16,11 @@ const Inventory: Component = () => {
 
     return (
         <div class="container mx-auto">
-        <div class="grid lg:grid-cols-2 md:grid-cols-1 gap-8">
-            {inventory().map((item) => (
-                <Card item={item}></Card>
-            ))}
-        </div>
+            <div class="grid lg:grid-cols-2 md:grid-cols-1 gap-8">
+                {inventory().map((item) => (
+                    <Card item={item}></Card>
+                ))}
+            </div>
         </div>
     );
 }
