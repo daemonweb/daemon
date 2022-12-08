5b614bc74c4fb2c03a79509e41853a02d3ab6c8a
Josh Elias <code.5jzcm@simplelogin.com>
2022-12-08 15:31:00 -0500

    connect strapi to cdk postgres

--- backend/edgy-admin/config/database.ts
+++ backend/edgy-admin/config/database.ts
@@ -2,12 +2,12 @@ export default ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '142.93.194.129'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'development'),
-      user: env('DATABASE_USERNAME', 'remote'),
-      password: env('DATABASE_PASSWORD', 'veggies'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL'),
     },
   },
 });
