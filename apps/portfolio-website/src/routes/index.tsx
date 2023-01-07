1f1008a19be2e46f8172b57376b8d207820e3859
Josh Elias <code.5jzcm@simplelogin.com>
2023-01-06 19:56:06 -0500

    add list view

--- apps/portfolio-website/src/routes/index.tsx
+++ apps/portfolio-website/src/routes/index.tsx
@@ -1,10 +1,11 @@
 import { A } from "solid-start";
 import Counter from "~/components/Counter";
+import ListView from "~/components/ListView";
 
 export default function Home() {
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
-      <h1>Index Page</h1>
+      <ListView/>
     </main>
   );
 }
