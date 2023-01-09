52efc10f5b80f8db592d00af522113311a9574a7
Josh Elias <code.5jzcm@simplelogin.com>
2023-01-09 14:59:19 -0500

    add types

---
+++ apps/portfolio-website/src/types.ts
@@ -0,0 +1,46 @@
+export const enum ProjectType {
+    Website = "Website",
+    App = "App",
+    SaaS = "SaaS",
+    Game = "Game",
+}
+
+export const enum ProjectFeature {
+    Blog = "Blog",
+    CMS = "CMS",
+    Ecommerce = "Ecommerce",
+    Localization = "Localization",
+    Newsletter = "Newsletter",
+    VideoHosting = "Video Hosting",
+    LeadGen = "Lead Generation",
+    UserManagement = "User Management",
+}
+
+export const enum ProjectTech {
+    React = "React",
+    SolidJS = "SolidJS",
+    Angular = "Angular",
+    Webflow = "Webflow",
+    Gatsby = "Gatsby",
+    Angular1 = "Angular1",
+    Angular2 = "Angular2",
+    Postgres = "Postgres",
+    AWS = "AWS",
+    NextJS = "NextJS",
+    LoopbackJS = "LoopbackJS",
+    MongoDB = "MongoDB",
+    Express = "Express",
+    NodeJS = "NodeJS",
+    Docker = "Docker",
+}
+
+export const enum ProjectRole {
+    Frontend = "Frontend",
+    Backend = "Backend",
+    Fullstack = "Fullstack",
+    Design = "Design",
+    ProjectManager = "Project Manager",
+    QA = "QA",
+    DevOps = "DevOps",
+    ProductOwner = "Product Owner",
+}
