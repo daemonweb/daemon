36bca0835890056c7d7bbbdbfeeb8b5dabdeef7f
Josh Elias <code.5jzcm@simplelogin.com>
2023-01-11 11:21:05 -0500

    refactor NavOption path matching

--- apps/portfolio-website/src/components/NavOption.tsx
+++ apps/portfolio-website/src/components/NavOption.tsx
@@ -1,6 +1,5 @@
 import { JSXElement } from "solid-js";
-import { A, useMatch } from "solid-start";
-import { combineClasses } from "../utils";
+import { A } from "solid-start";
 
 
 type NavOptionProps = {
@@ -10,17 +9,12 @@ type NavOptionProps = {
 }
 
 export default function NavOption(props: NavOptionProps) {
-    const match = useMatch(() => props.path);
-
     return (
         <A 
             href={props.path} 
-            class={combineClasses(
-                match()
-                    ? 'bg-indigo-800 text-white' 
-                    : 'text-indigo-100 hover:bg-indigo-600 hover:bg-opacity-75',
-                'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
-            )}
+            class="group flex items-center px-2 py-2 text-sm font-medium rounded-md"
+            activeClass="bg-indigo-800 text-white"
+            inactiveClass="text-indigo-100 hover:bg-indigo-600 hover:bg-opacity-75"
         >
             {props.icon}
             <span class="flex-1 ml-1.5">{props.title}</span>
